766bf53fd89cbbccd99d20d0b49f295c4ccee251
zzj <1076916122@qq.com>
2019-12-07 17:34:57 +0800

    tabbar初始化

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
+    <router-view/>
+    <main-tab-ber/>
   </div>
 </template>
 
 <script>
+  import MainTabBer from 'components/content/mainTabbar/MainTabBar.vue'
+
 export default {
   name: 'app',
   components: {
+    MainTabBer
   }
 }
 </script>
 
 <style>
-
+  @import "assets/css/base.css"
 </style>
