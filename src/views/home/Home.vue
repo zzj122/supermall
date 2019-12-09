f185a0040c3528c9aa807a38d40d5dd5877625d8
zhangzhijie <1076916122@qq.com>
2019-12-09 11:32:39 +0800

    流行、新款、精选组件的封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,24 +4,62 @@
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature-view/>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"/>
+
+    <ul>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+      <li>asdahjskfhaks</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childComps/HomeSwiper'
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
+
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
 
   import {getHomeMultidata} from 'network/home'
 
   export default {
     name: "Home",
     components: {
-      NavBar,
       HomeSwiper,
       RecommendView,
-      FeatureView
+      FeatureView,
+      NavBar,
+      TabControl
     },
     data() {
       return {
@@ -54,5 +92,10 @@
     right: 0;
     top: 0;
     z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
